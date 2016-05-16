594cbd41b87e74833d48dd8445ddd40a0217c554
annanormark <anna.nor94@gmail.com>
2016-05-16 16:55:55 +0200

    Timer settings saved

--- HHTimer/app/src/main/java/com/mikaela/hhtimer/Timer.java
+++ HHTimer/app/src/main/java/com/mikaela/hhtimer/Timer.java
@@ -10,7 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.lang.String;
 
@@ -31,20 +30,34 @@ public class Timer extends AppCompatActivity  {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.timer);
-
         studySession = (SeekBar) findViewById(R.id.StudyTime);
         breakSession = (SeekBar) findViewById(R.id.PauseTime);
         totalTime = (SeekBar) findViewById(R.id.TotalTime);
-        SharedPreferences sharedpreferences = getSharedPreferences(MyPrefrences, Context.MODE_PRIVATE);
+        setButton = (Button) findViewById(R.id.setBtn);
 
+        /*** SharedPrefrences setup ***/
+        SharedPreferences sharedpreferences = getSharedPreferences(MyPrefrences, Context.MODE_PRIVATE);
+        final SharedPreferences.Editor edit = sharedpreferences.edit();
+
+        /*** Numbers shown next to seekbar ***/
         final TextView studyNumber = (TextView) findViewById(R.id.StudySNumber);
         final TextView breakNumber = (TextView) findViewById(R.id.BreakSNumber);
         final TextView totalNumber = (TextView) findViewById(R.id.StudyNumber);
-        final SharedPreferences.Editor edit = sharedpreferences.edit();
+
+        /*** Sets progressed value in seekbar ***/
+        long studySes =  (sharedpreferences.getLong("studySession", 50*60000 + 1000) - 1000)/ 60000;
+        long breakSes =  (sharedpreferences.getLong("breakSession", 10*60000 + 1000) - 1000)/ 60000;
+        long total =  (sharedpreferences.getLong("totalTime", 120*60000 + 1000) - 1000) / 60000;
+        studySession.setProgress((int) studySes);
+        breakSession.setProgress((int) breakSes);
+        totalTime.setProgress((int) total);
+        studyNumber.setText("" + studySes + " min");
+        breakNumber.setText("" + breakSes + " min");
+        totalNumber.setText("" + total + " min");
 
 
+        /*** Sets seekbars ***/
         studySession.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
@@ -94,11 +107,12 @@ public class Timer extends AppCompatActivity  {
             }
         });
 
-        setButton = (Button) findViewById(R.id.setBtn);
+
         setButton.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
+                /*** Gets number from user input ***/
                 String studyString = studyNumber.getText().toString();
                 long sst = Long.parseLong(studyString.substring(0, studyString.length() - 4)) * 60000 + 1000;
                 String breakString = breakNumber.getText().toString();
@@ -106,15 +120,16 @@ public class Timer extends AppCompatActivity  {
                 String totaltString = totalNumber.getText().toString();
                 long tt = Long.parseLong(totaltString.substring(0, studyString.length() - 4)) * 60000 + 1000;
 
+                /*** Adds user input to DB ***/
                 edit.putLong("studySession", sst);
                 edit.putLong("breakSession", bst);
                 edit.putLong("totalTime", tt);
                 edit.commit();
 
+                /*** go to start-page ***/
                 Intent i = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(i);
             }
-
 
         });
 
